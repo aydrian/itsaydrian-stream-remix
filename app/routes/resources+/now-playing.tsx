5ebd94d1f7e7638fa4ed97ced83ead6c2b3e0f9e
Aydrian Howard <aydrian@gmail.com>
2023-05-30 22:50:50 -0400

    switched Now Playing to use SSE

--- app/routes/resources+/now-playing.tsx
+++ app/routes/resources+/now-playing.tsx
@@ -1,60 +1,60 @@
 import type { LoaderArgs } from "@remix-run/node";
-import { useEffect } from "react";
-import { json } from "@remix-run/node";
-import { useFetcher } from "@remix-run/react";
+import type { Song } from "~/utils/spotify.server";
+import { eventStream } from "remix-utils";
 import invariant from "tiny-invariant";
 import { getUsersNowPlaying } from "~/utils/spotify.server";
+import { useEventSource } from "remix-utils";
+
+const sleep = (ms: number) => new Promise((_) => setTimeout(_, ms));
 
 export const loader = async ({ request }: LoaderArgs) => {
-  // const url = new URL(request.url);
-  // const refreshToken = url.searchParams.get("refreshToken");
-  // TODO: Use function to pull from database or session
-  const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
-  invariant(typeof refreshToken === "string", "refreshToken is required");
-  const song = await getUsersNowPlaying(refreshToken);
+  return eventStream(request.signal, function setup(send) {
+    const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
+    invariant(typeof refreshToken === "string", "refreshToken is required");
+    let polling = true;
 
-  return json(song);
+    (async function doPolling() {
+      while (polling) {
+        let song = await getUsersNowPlaying(refreshToken);
+        if (!song.isPlaying) {
+          polling = false;
+          break;
+        }
+        send({ event: "now-playing", data: JSON.stringify(song) });
+        await sleep(song.duration - song.progress);
+      }
+    })();
+
+    return function clear() {
+      polling = false;
+    };
+  });
 };
 
 export function NowPlaying() {
-  const songFetcher = useFetcher<typeof loader>();
+  const songEvent = useEventSource("/resources/now-playing", {
+    event: "now-playing"
+  });
+  const song = songEvent ? (JSON.parse(songEvent) as Song) : undefined;
 
-  useEffect(() => {
-    songFetcher.load(`/resources/now-playing`);
-  }, []);
-
-  useEffect(() => {
-    if (!songFetcher.data?.isPlaying) {
-      return;
-    }
-    const delay = songFetcher.data.duration - songFetcher.data.progress;
-    const timer = setTimeout(() => {
-      songFetcher.load(`/resources/now-playing`);
-    }, delay);
-    return () => clearTimeout(timer);
-  }, [songFetcher.data]);
-
-  if (!songFetcher.data?.isPlaying) {
-    //return <div>Nothing is playing</div>;
+  if (!song?.isPlaying) {
     return null;
   }
 
-  const albumArt = songFetcher.data.images.find(({ height }) => height === 64);
+  const albumArt = song.images?.find(({ height }) => height === 64);
 
   return (
     <div className="flex items-center gap-1">
       {albumArt ? (
         <img
           src={albumArt.url}
-          alt={`${songFetcher.data.title} - ${songFetcher.data.artist}`}
+          alt={`${song.title} - ${song.artist}`}
           className="aspect-square h-[42px] rounded"
         />
       ) : null}
       <div className="flex flex-col">
-        <div className="font-bold leading-tight">{songFetcher.data.title}</div>
-        <div className="font-xs leading-tight text-gray-300">
-          {songFetcher.data.artist}
-        </div>
+        <div className="font-bold leading-tight">{song.title}</div>
+        <div className="font-xs leading-tight text-gray-300">{song.artist}</div>
       </div>
     </div>
   );

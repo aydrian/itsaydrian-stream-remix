a2f589034692ea3a1be22d9c9a7d36e9d9713552
Aydrian Howard <aydrian@gmail.com>
2023-10-15 08:23:07 -0400

    fix: temporary fix for getSearchParams not working

--- app/routes/scenes+/_layout.tsx
+++ app/routes/scenes+/_layout.tsx
@@ -1,21 +1,21 @@
 import { type LoaderFunctionArgs, json } from "@remix-run/node";
 import { Outlet, useRouteLoaderData } from "@remix-run/react";
-import { getSearchParams } from "remix-params-helper";
 import { z } from "zod";
 
 import { ScreenSizeSchema } from "~/components/screen-container";
+import { getSearchParams } from "~/utils/zod-helpers.server";
 
 const SearchParamsSchema = z.object({
   screenSize: ScreenSizeSchema.default("16:9"),
-  showGuides: z.coerce.boolean().default(false)
+  showGuides: z.boolean().default(false)
 });
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  console.log(new URL(request.url).searchParams);
   const result = getSearchParams(request, SearchParamsSchema);
   if (!result.success) {
     throw json(result.errors, { status: 400 });
   }
+  console.log({ result, url: request.url });
   const screenSize = result.data.screenSize;
   const showGuides = result.data.showGuides;
 
@@ -29,7 +29,6 @@ export function useOptions() {
       "useOptions must be used within the routes/scenes+/ route or its children"
     );
   }
-  console.log({ data });
   return data.options;
 }
 

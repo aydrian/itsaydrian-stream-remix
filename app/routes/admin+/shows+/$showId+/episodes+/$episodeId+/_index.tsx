202d8ff5b7e7c8d7ff6a1deaf9675c030cf6ad3f
Aydrian Howard <aydrian@gmail.com>
2023-10-17 07:48:34 -0400

    chore: refactor vdo generate links code

--- app/routes/admin+/shows+/$showId+/episodes+/$episodeId+/_index.tsx
+++ app/routes/admin+/shows+/$showId+/episodes+/$episodeId+/_index.tsx
@@ -14,12 +14,12 @@ import { DuplicateEpisodeForm } from "~/routes/resources+/episode-duplicate";
 import { requireUserId } from "~/utils/auth.server";
 import { prisma } from "~/utils/db.server";
 import { formatDateRange } from "~/utils/misc";
-import { generateVDOPassword } from "~/utils/vdo-ninja.server";
+import { generateDirectorUrl, getGuestLinks } from "~/utils/vdo-ninja.server";
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   await requireUserId(request);
   const { episodeId } = params;
-  const episode = await prisma.episode
+  const episodeResult = await prisma.episode
     .findUniqueOrThrow({
       select: {
         description: true,
@@ -46,32 +46,32 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
       console.error(error);
       throw new Response(null, { status: 404, statusText: "Not Found" });
     });
+  const { guests: guestResults, ...episode } = episodeResult;
 
-  const vdoConfig = {
-    hash: await generateVDOPassword(episode.vdoPassword),
-    password: episode.vdoPassword,
-    room: episode.show.title.toLowerCase().replace(/ /g, "_")
-  };
+  const room = episode.show.title.toLowerCase().replace(/ /g, "_");
+  const guests = await Promise.all(
+    guestResults.map(async ({ guest, order }) => ({
+      ...guest,
+      ...(await getGuestLinks(room, `Guest${order}`, episode.vdoPassword))
+    }))
+  );
 
-  return json({ ...episode, vdoConfig });
+  const directorUrl = generateDirectorUrl(room, episode.vdoPassword);
+
+  return json({ ...episode, directorUrl, guests });
 };
 
 export default function EpisodeIdIndex() {
   const {
     description,
+    directorUrl,
     endDate,
     guests,
     id,
     startDate,
     subtitle,
-    title,
-    vdoConfig
+    title
   } = useLoaderData<typeof loader>();
-
-  const directorSearchParams = new URLSearchParams([
-    ["director", vdoConfig.room],
-    ["password", vdoConfig.password]
-  ]);
 
   return (
     <>
@@ -93,82 +93,57 @@ export default function EpisodeIdIndex() {
             </CardHeader>
             <CardContent>
               <div className="flex flex-wrap gap-2">
-                {guests.map(({ guest, order }, index) => {
-                  const joinSearchParams = new URLSearchParams([
-                    ["hash", vdoConfig.hash],
-                    ["id", `Guest${order}`],
-                    ["r", vdoConfig.room]
-                  ]);
-                  const pushCameraSearchParams = new URLSearchParams([
-                    ["password", vdoConfig.password],
-                    ["room", vdoConfig.room],
-                    ["solo", ""],
-                    ["view", `Guest${order}`]
-                  ]);
-                  const pushScreenSearchParams = new URLSearchParams([
-                    ...Array.from(pushCameraSearchParams.entries()),
-                    ["view", `Guest${order}:s`]
-                  ]);
-                  return (
-                    <Card className="md:w-60" key={guest.id}>
-                      <CardHeader>
-                        <CardTitle>{`${guest.firstName} ${guest.lastName}`}</CardTitle>
-                        <CardDescription>
-                          {index === 0 ? "Host" : `Guest ${index}`}
-                        </CardDescription>
-                      </CardHeader>
-                      <CardContent>
-                        {index === 0 ? (
+                {guests.map((guest, index) => (
+                  <Card className="md:w-60" key={guest.id}>
+                    <CardHeader>
+                      <CardTitle>{`${guest.firstName} ${guest.lastName}`}</CardTitle>
+                      <CardDescription>
+                        {index === 0 ? "Host" : `Guest ${index}`}
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      {index === 0 ? (
+                        <div>
+                          Control Center:{" "}
+                          <pre className="text-xs">
+                            <a
+                              href={directorUrl}
+                              rel="noreferrer"
+                              target="_blank"
+                            >
+                              {directorUrl}
+                            </a>
+                          </pre>
+                        </div>
+                      ) : (
+                        <>
                           <div>
-                            Control Center:{" "}
+                            Push Camera Link:{" "}
                             <pre className="text-xs">
-                              <a
-                                href={`https://vdo.ninja/?${directorSearchParams.toString()}`}
-                                rel="noreferrer"
-                                target="_blank"
-                              >{`https://vdo.ninja/?${directorSearchParams}`}</a>
+                              <a href={guest.pushCameraUrl}>
+                                {guest.pushCameraUrl}
+                              </a>
                             </pre>
                           </div>
-                        ) : (
-                          <>
-                            <div>
-                              Push Camera Link:{" "}
-                              <pre className="text-xs">
-                                <a
-                                  href={`https://vdo.ninja/?${pushCameraSearchParams
-                                    .toString()
-                                    .replace(/=(?=&|$)/gm, "")}`}
-                                >{`https://vdo.ninja/?${pushCameraSearchParams
-                                  .toString()
-                                  .replace(/=(?=&|$)/gm, "")}`}</a>
-                              </pre>
-                            </div>
-                            <div>
-                              Push Screen Link:{" "}
-                              <pre className="text-xs">
-                                <a
-                                  href={`https://vdo.ninja/?${pushScreenSearchParams
-                                    .toString()
-                                    .replace(/=(?=&|$)/gm, "")}`}
-                                >{`https://vdo.ninja/?${pushScreenSearchParams
-                                  .toString()
-                                  .replace(/=(?=&|$)/gm, "")}`}</a>
-                              </pre>
-                            </div>
-                            <div>
-                              Join Link:{" "}
-                              <pre className="text-xs">
-                                <a
-                                  href={`https://vdo.ninja/?${joinSearchParams}`}
-                                >{`https://vdo.ninja/?${joinSearchParams}`}</a>
-                              </pre>
-                            </div>
-                          </>
-                        )}
-                      </CardContent>
-                    </Card>
-                  );
-                })}
+                          <div>
+                            Push Screen Link:{" "}
+                            <pre className="text-xs">
+                              <a href={guest.pushScreenUrl}>
+                                {guest.pushScreenUrl}
+                              </a>
+                            </pre>
+                          </div>
+                          <div>
+                            Join Link:{" "}
+                            <pre className="text-xs">
+                              <a href={guest.joinUrl}>{guest.joinUrl}</a>
+                            </pre>
+                          </div>
+                        </>
+                      )}
+                    </CardContent>
+                  </Card>
+                ))}
               </div>
             </CardContent>
           </Card>

3a7b0c48d1312a1c03a7d77bfafd82ebb91adaaf
Aydrian Howard <aydrian@gmail.com>
2023-11-24 13:35:52 -0500

    chore: package updates

--- scripts/test-subscribe.sh
+++ scripts/test-subscribe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# twitch event trigger subscribe -F https://stream.itsaydrian.com/resources/twitch/eventsub/itsaydrian \
-twitch event trigger subscribe -F http://localhost:3000/resources/twitch/eventsub/itsaydrian \
+# twitch event trigger subscribe -F http://localhost:3000/resources/twitch/eventsub/itsaydrian \
+twitch event trigger subscribe -F https://stream.itsaydrian.com/resources/twitch/eventsub/itsaydrian \
   -s purplemonkeydishwasher \
   -f 114823831 \
   -i "644b10b6-92ac-4f59-8baa-21c3b3cae5cb"

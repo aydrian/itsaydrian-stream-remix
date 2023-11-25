4c43d4903a9c4c53b5ca7c805ed22df8802c68b1
Aydrian Howard <aydrian@gmail.com>
2023-11-25 08:41:41 -0500

    chore: added local flag -l to scripts to target local dev

--- scripts/test-raid.sh
+++ scripts/test-raid.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-# twitch event trigger raid -F https://stream.itsaydrian.com/resources/twitch/eventsub/itsaydrian \
-twitch event trigger raid -F http://localhost:3000/resources/twitch/eventsub/itsaydrian \
+base_url = "https://stream.itsaydrian.com"
+
+while getopts 'l' flag; do
+  case "${flag}" in
+    l) base_url="http://localhost:3000" ;;
+  esac
+done
+
+twitch event trigger raid -F $base_url/resources/twitch/eventsub/itsaydrian \
   -s purplemonkeydishwasher \
   -f 114823831 \
   -i "644b10b6-92ac-4f59-8baa-21c3b3cae5cb"
--- scripts/test-reward-redemption.sh
+++ scripts/test-reward-redemption.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-twitch event trigger add-redemption -F http://localhost:3000/resources/twitch/eventsub/itsaydrian \
+base_url = "https://stream.itsaydrian.com"
+
+while getopts 'l' flag; do
+  case "${flag}" in
+    l) base_url="http://localhost:3000" ;;
+  esac
+done
+
+twitch event trigger add-redemption -F $base_url/resources/twitch/eventsub/itsaydrian \
   -s purplemonkeydishwasher \
   -f 114823831 \
   -i "644b10b6-92ac-4f59-8baa-21c3b3cae5cb" \
